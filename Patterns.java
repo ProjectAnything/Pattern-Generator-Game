49600e233c881f42f271395adee9d22bbd798f3f
Walid329 <40745115+Walid329@users.noreply.github.com>
2018-11-12 22:45:24 -0600

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Math;
+
+public class Patterns
+{
+	public static void main (String[] args) throws InterruptedException
+	{
+		System.out.println("Hello!");
+		System.out.println("Welcome to the Number Patterns Game.");
+		
+		int num = (int)(Math.random()*10+1);
+		
+		int operator = (int)(Math.random()*4);
+		
+		int start = 1;
+		
+		switch(operator) {
+		case 0:
+			start+=num;
+			break;
+		case 1:
+			start-=num;
+			break;
+		case 2:
+			start*=num;
+			break;
+		case 3:
+			start/=num;
+			break;
+		}
+			
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("insert guess here");
+		
+		int input = scan.nextInt();
+		
+		if (input == num) {
+			System.out.println("test");
+		}
+		}
+	}
